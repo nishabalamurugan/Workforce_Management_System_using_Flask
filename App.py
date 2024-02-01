5e83a3f5083ef78eff18d53d8cb4e5cb7db419f9
nishabalamurugan <96257304+nishabalamurugan@users.noreply.github.com>
2024-02-01 11:12:27 +0530

    Add files via upload

--- App.py
+++ App.py
@@ -10,11 +10,13 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['Employee_db']
 collection = db['Employee_collection']
 
-#routing
+#routing 
+#index page
 @app.route('/')
 def index():
     return render_template('Login.html')
 
+#authentication
 @app.route('/login',methods=['POST'])
 def login():
     User_name=request.form['username']
@@ -25,15 +27,18 @@ def login():
         error_message="OOPS! INVALID CREDENTIALS PROVIDED.\n Ensure that the username and password are correct."
         return render_template('Login.html',data=error_message)
 
+#display  all records
 @app.route('/employee_records')
 def Show_Employee_Records():
     data=list(collection.find())
     return render_template("Employee_Records.html",data=data)
 
+#inserting new record
 @app.route('/register_employee',methods=['GET'])
 def Register_Employee():
     return render_template("Register_Employee.html")
 
+#handling input from form data
 @app.route('/create',methods=['POST'])
 def Add_Employee_Data():
     New_Employee={
@@ -51,11 +56,13 @@ def Add_Employee_Data():
     collection.insert_one(New_Employee)
     return redirect('/employee_records');
 
+#update existing records
 @app.route('/edit/<id>',methods=['GET'])
 def Edit_Employee_Data(id):
     Data_to_be_edited=collection.find_one({'Employee_Id':id})
     return render_template('Edit_Employee_Data.html',data=Data_to_be_edited)
 
+#fetching existing record for updating
 @app.route('/update/<id>',methods=['POST'])
 def Update_Employee_Data(id):
     Updated_Data={
@@ -73,17 +80,20 @@ def Update_Employee_Data(id):
     collection.update_one({'Employee_Id':id},{'$set':Updated_Data})
     return redirect('/employee_records');
 
+#remove record
 @app.route('/delete/<id>')
 def Delete_Employee_Data(id):
     collection.delete_one({'Employee_Id':id})
     return redirect('/employee_records');
 
+#filter based on employee id
 @app.route('/filterbyemployeeid',methods=['POST'])
 def  FilterByEmployeeId():
     emp_id=request.form['employee_id']
     item=list(collection.find({"Employee_Id":emp_id}));
     return render_template('Employee_Records.html',data=item)
 
+#filter based on job id
 @app.route('/filterbyjobid',methods=['POST'])
 def FilterByJobId():
     job_id=request.form['job_id']
